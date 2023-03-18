c3e5a7b29445ac5deef5b093576833796b0b1521
zalper <auzgec@gmail.com>
2023-03-18 15:20:07 +0300

    Fix for hashmap and quiz2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -35,12 +35,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        scores.entry(team_1_name.clone()) // Update Team 1
+        .and_modify(|t| { // t: Team
+            t.goals_scored += team_1_score;
+            t.goals_conceded += team_2_score;
+        })
+        .or_insert(Team {name: team_1_name, goals_scored: team_1_score, goals_conceded: team_2_score});
+    scores.entry(team_2_name.clone()) // Update Team 2
+        .and_modify(|t| { 
+            t.goals_scored += team_2_score; 
+            t.goals_conceded += team_1_score; 
+        })
+        .or_insert(Team {name: team_2_name, goals_scored: team_2_score, goals_conceded: team_1_score});
     }
+
     scores
 }
 
