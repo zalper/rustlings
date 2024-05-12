4cfc204cab7ab18bcad748918893833d7ba68d66
zalper <auzgec@gmail.com>
2024-05-12 16:02:14 +0300

    fix quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,20 +27,16 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
     pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
-
         let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-
             match command {
                 Command::Uppercase => output.push(string.to_uppercase()),
                 Command::Trim => output.push(string.trim().to_string()),
-                Command::Append(num) => output.push(
-                    string.to_string() + &(String::from("bar").repeat(num.clone()))
-                )
+                Command::Append(num) => {
+                    output.push(string.to_string() + &(String::from("bar").repeat(num.clone())))
+                }
             }
-
         }
         output
     }
@@ -51,8 +45,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
 
-    use my_module::transformer;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
